777f38a0ac673d2bf85f0a355e0a3a99f65ee0f7
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-02-03 23:10:14 +0100

    reducers

---
+++ src/redux/reducers/tenant-reducer.js
@@ -0,0 +1,14 @@
+
+const INITIAL_STATE = {
+    tenants = []
+};
+
+export const tenantReducer (state = INITIAL_STATE, action) =>  {
+    switch (action.type) {
+        case 'GET_ALL_TENANTS':
+            break;
+
+        default:
+            return state;
+    }
+}
