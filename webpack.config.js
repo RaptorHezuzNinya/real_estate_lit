103318fdc2185affff6383550c83c891dfb8cf67
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-02-21 00:09:54 +0100

    add webconfig

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+	devServer: {
+		historyApiFallback: true
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', { targets: { ie: '11' } }]],
+						plugins: ['@babel/plugin-syntax-dynamic-import']
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			'images/**',
+			'node_modules/@webcomponents/webcomponentsjs/**',
+			'manifest.json'
+		]),
+		new HtmlWebpackPlugin({
+			chunksSortMode: 'none',
+			template: 'index.html'
+		}),
+		new WorkboxWebpackPlugin.GenerateSW({
+			include: ['index.html', 'manifest.json', /\.js$/],
+			exclude: [/\/@webcomponents\/webcomponentsjs\//],
+			navigateFallback: 'index.html',
+			swDest: 'service-worker.js',
+			clientsClaim: true,
+			skipWaiting: true,
+			runtimeCaching: [
+				{
+					urlPattern: /\/@webcomponents\/webcomponentsjs\//,
+					handler: 'staleWhileRevalidate'
+				},
+				{
+					urlPattern: /^https:\/\/fonts.gstatic.com\//,
+					handler: 'staleWhileRevalidate'
+				}
+			]
+		})
+	]
+};
