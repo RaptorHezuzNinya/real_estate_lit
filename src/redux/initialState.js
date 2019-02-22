b81d8984470e56e749b1088bed4911299f7d8eaf
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-02-22 13:50:26 +0100

    create initial state file

---
+++ src/redux/initialState.js
@@ -0,0 +1,14 @@
+export const INITIAL_STATE = {
+	tenants: [
+		{
+			account_holder: 'UNICORN',
+			email: 'unicorn@hotmail.com',
+			first_name: 'unicorn',
+			iban: 'NL08INGB0945874518',
+			id: 11,
+			last_name: 'unicorn',
+			phone: '0612345678',
+			rent: 747.16
+		}
+	]
+};
