dd38b262938123b6f7c53655615034bac79f480f
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-03-08 17:32:01 +0100

    add root reducer file'

---
+++ src/redux/actions/user.js
@@ -0,0 +1,4 @@
+export const setTenants = tenants => ({
+		type: 'SET_TENANTS',
+		payload: tenants
+	});
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { tenantReducer } from './tenant-reducer';
+
+export const rootReducer = combineReducers({
+	tenantReducer
+});
+
+// export default rootReducer;
