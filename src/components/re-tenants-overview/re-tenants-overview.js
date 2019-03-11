7a4e3b5a6dfb6dd48b0b9e1a9a00fbc4fee5aecf
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-03-11 17:01:39 +0100

    overview component for tenants

---
+++ src/components/re-tenants-overview/re-tenants-overview.js
@@ -0,0 +1,41 @@
+import { LitElement, html } from 'lit-element';
+import { connect } from 'pwa-helpers';
+import { store } from '../../redux/store';
+import { ReTenantsOverViewStyles } from './re-tenants-overview-styles';
+import { getTenants } from '../../redux/actions/tenant';
+import '../re-tenant/re-tenant';
+// import { NormalizeCss } from '../re-normalize-css/re-normalize-css';
+
+class ReTenantsOverView extends connect(store)(LitElement) {
+	static get properties() {
+		return {
+			tenants: { type: Array }
+		};
+	}
+
+	constructor() {
+		super();
+		store.dispatch(getTenants());
+	}
+
+	render() {
+		const { tenants } = this;
+
+		return html`
+			<ul>
+				${tenants.map(
+					tenant =>
+						html`
+							<re-tenant .tenant=${tenant}></re-tenant>
+						`
+				)}
+			</ul>
+		`;
+	}
+
+	stateChanged(state) {
+		this.tenants = state.tenantReducer.tenants;
+	}
+}
+
+customElements.define('re-tenants-overview', ReTenantsOverView);
