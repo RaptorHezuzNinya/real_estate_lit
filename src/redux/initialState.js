c3a4031dd94eb0894499b548a8d24ce306434e45
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-03-11 17:00:39 +0100

    fixed reducer

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,22 +1,9 @@
-export const INITIAL_STATE = {
-	tenants: [
-		{
-			account_holder: 'UNICORN',
-			email: 'unicorn@hotmail.com',
-			first_name: 'unicorn',
-			iban: 'NL08INGB0945874518',
-			id: 11,
-			last_name: 'unicorn',
-			phone: '0612345678',
-			rent: 747.16
-		}
-	],
-	books: [
-		{ title: 'harry potter', isbn: '1231245654', pages: 652 },
-		{ title: 'Alice in wonderland', isbn: '1231286754', pages: 652 }
-	],
-	users: [
-		{ username: 'ward.verhoef', age: '14', profileId: 212, active: true },
-		{ username: 'w.v101', age: '14', profileId: 654, active: true }
-	]
-};
+// {
+// 	harrypotter: { title: 'HarryPotter', isbn: '1231245654', pages: 652 },
+// 	aliceinwonderland: { title: 'Alice in wonderland', isbn: '1231286754', pages: 652 }
+
+// }
+// users: [
+// 	{ username: 'ward.verhoef', age: '14', profileId: 212, active: true },
+// 	{ username: 'w.v101', age: '14', profileId: 654, active: true }
+// ]
--- src/redux/reducers/tenant-reducer.js
+++ src/redux/reducers/tenant-reducer.js
@@ -1,15 +1,29 @@
 import { GET_TENANTS, SET_TENANTS } from '../actions/tenant';
-import { INITIAL_STATE } from '../initialState';
+
+const INITIAL_STATE = {
+	tenants: [
+		{
+			account_holder: 'Unicorn',
+			email: 'unicorn@hotmail.com',
+			first_name: 'unicorn',
+			iban: 'NL08INGB0945874518',
+			id: 11,
+			last_name: 'unicorn',
+			phone: '0612345678',
+			rent: 747.16
+		}
+	]
+};
 
 export const tenantReducer = (state = INITIAL_STATE, { type, payload }) => {
 	switch (type) {
 		case GET_TENANTS:
 			break;
+
 		case SET_TENANTS: {
-			return {
-				...state,
-				payload
-			};
+			const tenants = payload.map(tenant => tenant);
+			const updatedState = tenants.concat(Object.assign({}, ...state.tenants));
+			return { tenants: updatedState };
 		}
 		default:
 			return state;
