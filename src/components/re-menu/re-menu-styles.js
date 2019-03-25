3196c6b6175e8974ade072491340c61c677ef80d
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-03-25 09:57:27 +0100

    remove comments

--- src/components/re-menu/re-menu-styles.js
+++ src/components/re-menu/re-menu-styles.js
@@ -1,20 +1,9 @@
 import { html } from 'lit-element';
 
-// when exported with default this is import statement:
-// import menuComponentStyles from './menu-component-styles';
 export const ReMenuStyles = html`
 	<style>
-		.main-holder {
+		.textClass {
+			color: red;
 		}
 	</style>
 `;
-
-// when exported with named export import statement:
-// import { menuComponentStyles } from './menu-component-styles';
-// export const menuComponentStyles = html`
-// 	<style>
-// 		.main-holder {
-// 			background-color: green;
-// 		}
-// 	</style>
-// `;
