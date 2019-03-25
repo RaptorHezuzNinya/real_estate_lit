ad6815e04e10e18e81f626a6ef824804f72e1ea0
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-03-25 10:01:22 +0100

    refactor remenu

--- src/components/re-menu/re-menu.js
+++ src/components/re-menu/re-menu.js
@@ -2,20 +2,33 @@ import { LitElement, html, css } from 'lit-element';
 import { connect } from 'pwa-helpers';
 import { store } from '../../redux/store';
 import { ReMenuStyles } from './re-menu-styles';
+import { normalize } from '../../normalize';
 import '../re-tenants-overview/re-tenants-overview';
-// import { normalize } from '../../normalize';
+import '../re-my-element/re-my-element';
 
 class ReMenu extends connect(store)(LitElement) {
-	// static get properties() {
-	// 	return css`
-	// 		${normalize}
-	// 	`;
-	// }
+	static get styles() {
+		return [
+			css`
+				${normalize}
+			`
+			// css`
+			// 	:host {
+			// 		display: block;
+			// 	}
+			// `
+		];
+	}
 
 	render() {
 		return html`
 			<main class="main-holder">
-				<re-tenants-overview></re-tenants-overview>
+				<h1>header in re-menu</h1>
+				<re-my-element>
+					<br />
+					<h1>Header ** AS SLOT ** in reMyElement</h1>
+					<p class="textClass">paragraph as slot</p>
+				</re-my-element>
 			</main>
 		`;
 	}
