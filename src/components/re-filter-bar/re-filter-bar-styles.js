674e7a9dbe895fc58a71346cf27e006d32700a1e
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-04-01 00:58:29 +0200

    new filter bar holder for filters

---
+++ src/components/re-filter-bar/re-filter-bar-styles.js
@@ -0,0 +1,9 @@
+import { css } from 'lit-element';
+
+export const filterBarStyles = css`
+	.filter-bar {
+		background-color: green;
+		height: 55px;
+		width: 100%;
+	}
+`;
---
+++ src/components/re-filter-bar/re-filter-bar.js
@@ -0,0 +1,47 @@
+import { LitElement, html, css } from 'lit-element';
+import { normalize } from '../../css/normalize';
+import { filterBarStyles } from './re-filter-bar-styles';
+
+class ReFilterBar extends LitElement {
+	static get styles() {
+		return [
+			css`
+				${normalize}
+			`,
+			css`
+				${filterBarStyles}
+			`
+		];
+	}
+
+	constructor() {
+		super();
+		this.months = [
+			{ 1: 'January' },
+			{ 2: 'February' },
+			{ 3: 'March' },
+			{ 4: 'April' },
+			{ 5: 'May' },
+			{ 6: 'June' },
+			{ 7: 'July' },
+			{ 8: 'August' },
+			{ 9: 'September' },
+			{ 10: 'October' },
+			{ 11: 'November' },
+			{ 12: 'December' }
+		];
+		this.years = [{ 2015: '2015' }, { 2016: '2016' }, { 2017: '2017' }, { 2018: '2018' }];
+	}
+
+	render() {
+		const { months, years } = this;
+		return html`
+			<div class="filter-bar">
+				<re-select-box .selectBoxOptions=${years}></re-select-box>
+				<re-select-box .selectBoxOptions=${months}></re-select-box>
+			</div>
+		`;
+	}
+}
+
+customElements.define('re-filter-bar', ReFilterBar);
