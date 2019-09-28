b2fa0b3783b67578414ba997b3f8dde30477a4d1
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-09-28 23:51:11 +0200

    new form reducer

---
+++ src/redux/reducers/formReducer.js
@@ -0,0 +1,17 @@
+// import { SUBMIT_FORM } from '../actions/form';
+
+// const INITIAL_STATE = {};
+
+// export const formReducer = (state = INITIAL_STATE, { type, payload }) => {
+// 	switch (type) {
+// 		case SUBMIT_FORM:
+
+// 		case SET_TENANTS: {
+// 			const tenants = payload.map(tenant => tenant);
+// 			const updatedState = tenants.concat(Object.assign({}, ...state.tenants));
+// 			return { tenants: updatedState };
+// 		}
+// 		default:
+// 			return state;
+// 	}
+// };
