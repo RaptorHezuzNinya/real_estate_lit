f5d17fd41a977d8fa6c9e39d6da05f26e06dafe1
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-09-29 18:14:41 +0200

    fix imports and renders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime';
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
-import './components/my-app';
+import './components/re-app/re-app.js';
 import './assets/css/fonts.css';
 
 if (process.env.NODE_ENV !== 'production') {
