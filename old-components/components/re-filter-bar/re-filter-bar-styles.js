2cdd5259fc68fa4e32b5c3e8ed16fab1b5c99875
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-09-29 20:45:43 +0200

    removed

--- old-components/components/re-filter-bar/re-filter-bar-styles.js
+++
@@ -1,9 +0,0 @@
-import { css } from 'lit-element';
-
-export const filterBarStyles = css`
-	.filter-bar {
-		background-color: green;
-		height: 55px;
-		width: 100%;
-	}
-`;
--- old-components/components/re-menu/re-menu-styles.js
+++
@@ -1,7 +0,0 @@
-import { css } from 'lit-element';
-
-export const ReMenuStyles = css`
-	.textClass {
-		color: red;
-	}
-`;
