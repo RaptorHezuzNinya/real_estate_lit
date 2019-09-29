119dd5e95fcabb30bf042eb6cb59f94ddc292291
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-09-29 21:16:57 +0200

    add mini req

--- src/components/re-home-page/re-home-page.js
+++ src/components/re-home-page/re-home-page.js
@@ -3,12 +3,24 @@ import { connect } from 'pwa-helpers';
 import { store } from '../../redux/store';
 import { PageViewElement } from '../page-view-element.js';
 import { SharedStyles } from '../shared-styles.js';
-import '../can-button-v2/can-button-v2.js';
 import { fetchTenants } from '../../redux/actions/tenant';
+import '../can-button-v2/can-button-v2.js';
+import '../re-tenant-card/re-tenant-card.js';
 
 class ReHomePage extends connect(store)(PageViewElement) {
 	static get styles() {
 		return [SharedStyles];
+	}
+
+	static get properties() {
+		return {
+			tenants: { type: Object }
+		};
+	}
+
+	constructor() {
+		super();
+		this.tenants = false;
 	}
 
 	connectedCallback() {
@@ -29,11 +41,16 @@ class ReHomePage extends connect(store)(PageViewElement) {
 			</section>
 			<section>
 				<p>section 2</p>
+				<re-tenant-card></re-tenant-card>
 			</section>
 			<section>
 				<p>section 3</p>
 			</section>
 		`;
+	}
+
+	stateChanged(state) {
+		this.tenants = state.tenant.tenants;
 	}
 }
 
