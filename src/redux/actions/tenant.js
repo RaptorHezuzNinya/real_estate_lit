0242e52a91763238059ab128797c486790b7353f
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-09-29 18:38:03 +0200

    actions for tenant

---
+++ src/redux/actions/tenant.js
@@ -0,0 +1,7 @@
+export const FETCH_TENANTS = 'FETCH_TENANTS';
+export const FETCH_TENANTS_SUCCESS = 'FETCH_TENANTS_SUCCESS';
+export const FETCH_TENANTS_ERROR = 'FETCH_TENANTS_ERROR';
+
+export const fetchTenants = () => ({
+	type: FETCH_TENANTS
+});
