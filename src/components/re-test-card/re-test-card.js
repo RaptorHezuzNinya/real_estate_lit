de2ac19117409adaf8b8960a12693e05e359ac1a
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-04 02:07:56 +0200

    new card component

---
+++ src/components/re-test-card/re-test-card.js
@@ -0,0 +1,58 @@
+import { LitElement, html } from 'lit-element';
+import { ReTestCardStyles } from './re-test-card-styles.js';
+
+export class ReTestCard extends LitElement {
+	static get styles() {
+		return [ReTestCardStyles];
+	}
+	static get properties() {
+		return {
+			cardIsActive: { type: Boolean },
+			tenant: Object
+		};
+	}
+	constructor() {
+		super();
+		this.cardIsActive = false;
+		this.tenant = false;
+	}
+
+	render() {
+		return html`
+			<div
+				class=${`card  ${this.cardIsActive ? 'cardActive' : null}`}
+				@click=${this.handleCardActivation}
+			>
+				<section id="cardHeader">
+					<span class="cardAvatarContainer">
+						<span class="cardAvatar"></span>
+					</span>
+					<span class="cardHeaderTextContainer">
+						<div class="cardHeaderTitle"></div>
+						<div class="cardHeaderSupportingText">${this.tenant.email}</div>
+					</span>
+				</section>
+
+				<section class="nonSharedContent">
+					<section class="greySquaresContainer">
+						<span class="greySquare"></span>
+						<span class="greySquare"></span>
+						<span class="greySquare"></span>
+						<span class="greySquare"></span>
+					</section>
+					<div class="greyLine"></div>
+					<div class="greyLine"></div>
+					<div class="greyLine"></div>
+					<div class="greyLine"></div>
+					<div class="greyLine"></div>
+					<div class="greyLine"></div>
+				</section>
+			</div>
+		`;
+	}
+
+	handleCardActivation() {
+		this.cardIsActive = !this.cardIsActive;
+	}
+}
+customElements.define('re-test-card', ReTestCard);
