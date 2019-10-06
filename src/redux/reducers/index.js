a2e024d7f0f2da53d24dd3c3df331c5a8d9091a8
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-07 00:30:39 +0200

    add new reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import { app } from './app.js';
 import { ui } from './ui.js';
 import { tenant } from './tenant.js';
 import { payment } from './payment.js';
+import { userReducer } from './user.reducer.js';
 
 const appReducer = combineReducers({
 	app,
 	ui,
 	tenant,
-	payment
+	payment,
+	user: userReducer
 });
 
 export const rootReducer = (state, action) => {
