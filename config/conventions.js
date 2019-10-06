c194754e6141c28061151a11511866593a4ce1b8
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-07 00:31:15 +0200

    updated with conventions file'

---
+++ config/conventions.js
@@ -0,0 +1,4 @@
+// new custom Event -> always TYPE - WHAT - TYPE of EVT
+const newEvent1 = new CustomEvent('input-value-changed');
+const newEvent2 = new CustomEvent('input-focus-out');
+const newEvent3 = new CustomEvent('button-click');
