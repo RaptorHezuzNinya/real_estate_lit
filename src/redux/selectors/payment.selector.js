27866e6fb38fbdb1a35a9ec8881d2628c1bef355
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-06 20:04:39 +0200

    new paymenty selector

---
+++ src/redux/selectors/payment.selector.js
@@ -0,0 +1,22 @@
+import { createSelector } from 'reselect';
+
+const paymentsSelector = state => state.payment.payments;
+const tenantsSelector = state => state.tenant.tenants;
+
+export const paymentsByTenantId = createSelector(
+	[paymentsSelector, tenantsSelector],
+
+	(payments, tenants) => {
+		const data = {};
+		for (const key in tenants) {
+			if (tenants.hasOwnProperty(key)) {
+				const paymentsByTenantId = payments.filter(filterCallBack, tenants[key]);
+				data[tenants[key].id] = paymentsByTenantId;
+			}
+		}
+		return data;
+	}
+);
+function filterCallBack(element) {
+	return element.tenant_id === this.id;
+}
