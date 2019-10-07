f3b7b5de12c36fbdd6927e6d5d780759d0cc02d0
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-08 00:30:25 +0200

    new core mw

--- src/redux/middleware/core/api.mw.js
+++ src/redux/middleware/core/api.mw.js
@@ -4,17 +4,13 @@ import { API_REQUEST, apiError, apiSuccess } from '../../actions/api.acs.js';
 
 export const apiMiddleware = ({ dispatch }) => next => action => {
 	next(action);
-	if (action.type.includes(API_REQUEST)) {
-		const { body, url, method, feature, headers } = action.meta;
-		// 	const { url, method, onSuccess, onFailure, label, headers } = action.meta;
 
-		// 	const data = action.payload;
-		// const dataOrParams = ['GET', 'DELETE'].includes(method) ? 'params' : 'data';
+	if (action.type.includes(API_REQUEST)) {
+		const { url, method, entity, headers } = action.meta;
+		const body = action.payload;
 
 		axios.defaults.baseURL = API_ROOT;
 		axios.defaults.headers.common['Content-Type'] = 'application/json';
-
-		fetch(url, { body, method });
 
 		axios
 			.request({
@@ -24,22 +20,7 @@ export const apiMiddleware = ({ dispatch }) => next => action => {
 				body
 			})
 			.then(response => response.json())
-			.then(response => dispatch(apiSuccess({ response, feature })))
-			.catch(error => dispatch(apiError({ error: error, feature })));
-
-		// .then(({ data }) => {
-		// 	if (label) dispatch(apiRequestEnd(label));
-		// 	dispatch({ type: onSuccess, payload: data });
-		// })
-		// .catch(error => {
-		// 	console.log('error', error);
-		// 	console.log('errror.response', error.response);
-		// 	if (label) dispatch(apiRequestEnd(label));
-		// 	dispatch({ type: onFailure, error: error.response });
-
-		// 	if (error.response && error.response.status === 403) {
-		// 		dispatch(accessDenied(window.location.pathname));
-		// 	}
-		// });
+			.then(response => dispatch(apiSuccess({ response, entity })))
+			.catch(error => dispatch(apiError({ error: error, entity })));
 	}
 };
