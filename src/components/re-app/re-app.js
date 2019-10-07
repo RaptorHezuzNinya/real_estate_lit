19cd7a61bae51b9f217856a6f823771f68243903
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-08 01:44:56 +0200

    import and markup

--- src/components/re-app/re-app.js
+++ src/components/re-app/re-app.js
@@ -8,7 +8,6 @@ import { installRouter } from 'pwa-helpers/router.js';
 import { updateMetadata } from 'pwa-helpers/metadata.js';
 import { navigate, updateOffline, updateDrawerState } from '../../redux/actions/app.js';
 import { ReAppStyles } from './re-app-styles';
-import { normalize } from '../../assets/css/normalize';
 import '@polymer/app-layout/app-drawer/app-drawer.js';
 import '@polymer/app-layout/app-header/app-header.js';
 import '../re-snack-bar/re-snack-bar.js';
@@ -19,39 +18,17 @@ import '../re-home-page/re-home-page.js';
 
 class ReApp extends connect(store)(LitElement) {
 	static get styles() {
-		return [normalize, ReAppStyles];
+		return [ReAppStyles];
 	}
 	static get properties() {
 		return {
 			appTitle: { type: String },
-			_page: { type: String },
-			_drawerOpened: { type: Boolean },
-			_snackbarOpened: { type: Boolean },
-			_offline: { type: Boolean }
+			page: { type: String },
+			drawerOpened: { type: Boolean },
+			snackbarOpened: { type: Boolean },
+			offline: { type: Boolean },
+			user: Object
 		};
-	}
-
-	render() {
-		return html`
-			<can-header></can-header>
-
-			<app-drawer .opened="${this._drawerOpened}" @opened-changed="${this._drawerOpenedChanged}">
-				<nav class="drawer-list">
-					<a ?selected="${this._page === 'register'}" href="/register">Sign up</a>
-				</nav>
-			</app-drawer>
-
-			<!-- Main content -->
-			<main role="main" class="main-content">
-				<re-home-page class="page" ?active="${this._page === 're-home-page'}"></re-home-page>
-				<re-register-page class="page" ?active="${this._page === 'register'}"></re-register-page>
-				<re-404 class="page" ?active="${this._page === 're-404'}"></re-404>
-			</main>
-
-			<snack-bar ?active="${this._snackbarOpened}">
-				You are now ${this._offline ? 'offline' : 'online'}.
-			</snack-bar>
-		`;
 	}
 
 	constructor() {
@@ -59,6 +36,30 @@ class ReApp extends connect(store)(LitElement) {
 		// To force all event listeners for gestures to be passive.
 		// See https://www.polymer-project.org/3.0/docs/devguide/settings#setting-passive-touch-gestures
 		setPassiveTouchGestures(true);
+		this.user = false;
+	}
+
+	render() {
+		return html`
+			<can-header></can-header>
+
+			<app-drawer .opened="${this.drawerOpened}" @opened-changed="${this._drawerOpenedChanged}">
+				<nav class="drawer-list">
+					<a ?selected="${this.page === 'register'}" href="/register">Sign up</a>
+				</nav>
+			</app-drawer>
+
+			<!-- Main content -->
+			<main role="main" class="main-content">
+				<re-home-page class="page" ?active="${this.page === 're-home-page'}"></re-home-page>
+				<re-register-page class="page" ?active="${this.page === 'register'}"></re-register-page>
+				<re-404 class="page" ?active="${this.page === 're-404'}"></re-404>
+			</main>
+
+			<snack-bar ?active="${this.snackbarOpened}">
+				You are now ${this.offline ? 'offline' : 'online'}.
+			</snack-bar>
+		`;
 	}
 
 	firstUpdated() {
