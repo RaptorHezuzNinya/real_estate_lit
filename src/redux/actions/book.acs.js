33740ad18b71b4afa662a0340ac66f8725ade921
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-08 01:44:31 +0200

    add acs

---
+++ src/redux/actions/book.acs.js
@@ -0,0 +1,18 @@
+// entity name
+export const BOOKS = '[Books]';
+
+// action types
+export const FETCH_BOOKS = `${BOOKS} FETCH`;
+export const SET_BOOKS = `${BOOKS} SET`;
+
+// action creators
+export const fetchBooks = ({ query }) => ({
+	type: FETCH_BOOKS,
+	payload: query
+});
+
+export const setBooks = ({ books, normalizeKey }) => ({
+	type: SET_BOOKS,
+	payload: books,
+	meta: { normalizeKey, entity: BOOKS }
+});
