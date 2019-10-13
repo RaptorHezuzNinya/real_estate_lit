6f4fbeb215223255aed1ed0ffbbf7c255fa5c2fb
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-14 00:52:59 +0200

    new ac "

--- src/redux/middleware/entity/user.mw.js
+++ src/redux/middleware/entity/user.mw.js
@@ -1,4 +1,4 @@
-import { USER, SIGN_UP_USER, setUser, LOGIN_USER } from '../../actions/user.acs.js';
+import { USER, SIGN_UP_USER, setUser, LOGIN_USER, FETCH_USER } from '../../actions/user.acs.js';
 import { API_ERROR, API_SUCCESS, apiRequest } from '../../actions/api.acs.js';
 import { setLoader } from '../../actions/ui.acs.js';
 import { setNotification } from '../../actions/notification.acs.js';
@@ -19,6 +19,14 @@ export const userMiddleware = ({ dispatch, getState }) => next => action => {
 		case LOGIN_USER: {
 			next([
 				apiRequest({ body: action.payload, method: 'POST', url: `/api/users/login`, entity: USER }),
+				setLoader({ state: true, entity: USER })
+			]);
+			break;
+		}
+
+		case FETCH_USER: {
+			next([
+				apiRequest({ url: `/api/user`, entity: USER, auth: true }),
 				setLoader({ state: true, entity: USER })
 			]);
 			break;
