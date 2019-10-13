994c555ee3d899df4471214ac8c7240b93f529a4
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-13 23:46:04 +0200

    new tab bar comp

---
+++ src/components/re-tab-bar/re-tab-bar-styles.js
@@ -0,0 +1,7 @@
+import { css } from 'lit-element';
+
+export const ReTabBarStyles = css`
+	:host {
+		display: block;
+	}
+`;
---
+++ src/components/re-tab-bar/re-tab-bar.js
@@ -0,0 +1,75 @@
+import { LitElement, html, unsafeCSS } from 'lit-element';
+import { ReTabBarStyles } from './re-tab-bar-styles.js';
+import MDCTabBarStyles from './re-tab-bar-styles.scss';
+import { MDCTabBar } from '@material/tab-bar';
+
+export class ReTabBar extends LitElement {
+	static get styles() {
+		return [ReTabBarStyles, unsafeCSS(MDCTabBarStyles)];
+	}
+
+	static get properties() {
+		return {
+			MDCTabBar: { type: Object }
+			// MDCTabsBarElement: { type: Object }
+		};
+	}
+
+	constructor() {
+		super();
+		this.MDCTabBar = false;
+		// this.MDCTabsBarElement = false;
+	}
+
+	firstUpdated() {
+		this.MDCTabBar = new MDCTabBar(this.shadowRoot.querySelector('.mdc-tab-bar'));
+		this.MDCTabBar.listen('MDCTabBar:activated', this.activeTabChanged.bind(this));
+	}
+	render() {
+		return html`
+			<div class="mdc-tab-bar" role="tablist">
+				<div class="mdc-tab-scroller">
+					<div class="mdc-tab-scroller__scroll-area">
+						<div class="mdc-tab-scroller__scroll-content">
+							<button class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0">
+								<span class="mdc-tab__content">
+									<!-- <span class="mdc-tab__icon material-icons" aria-hidden="true">favorite</span> -->
+									<span class="mdc-tab__text-label">Tenant Overview</span>
+								</span>
+								<span class="mdc-tab-indicator mdc-tab-indicator--active">
+									<span
+										class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"
+									></span>
+								</span>
+								<span class="mdc-tab__ripple"></span>
+							</button>
+							<button class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="1">
+								<span class="mdc-tab__content">
+									<!-- <span class="mdc-tab__icon material-icons" aria-hidden="true">favorite</span> -->
+									<span class="mdc-tab__text-label">Tenant Create</span>
+								</span>
+								<span class="mdc-tab-indicator mdc-tab-indicator--active">
+									<span
+										class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"
+									></span>
+								</span>
+								<span class="mdc-tab__ripple"></span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		`;
+	}
+
+	activeTabChanged(evt) {
+		const event = new CustomEvent('re-active-tab-change', {
+			detail: {
+				value: evt.detail.index
+			}
+		});
+		this.dispatchEvent(event);
+	}
+}
+
+customElements.define('re-tab-bar', ReTabBar);
