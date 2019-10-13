3776005f3dfdb3ab0a3374b6f523d914fc5d9609
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-14 00:51:28 +0200

    action for setting 1 tenant

--- src/redux/actions/tenant.acs.js
+++ src/redux/actions/tenant.acs.js
@@ -4,6 +4,7 @@ export const TENANTS = '[Tenants]';
 
 export const FETCH_TENANTS = `${TENANTS} ${FETCH}`;
 export const SET_TENANTS = `${TENANTS} ${SET}`;
+export const SET_TENANT = `${TENANTS} ${SET}`;
 export const CREATE_TENANTS = `${TENANTS} ${CREATE}`;
 
 export const fetchTenants = () => ({
@@ -11,12 +12,17 @@ export const fetchTenants = () => ({
 	meta: { entity: TENANTS }
 });
 
-export const setTenants = data => ({
+export const setTenants = tenants => ({
 	type: SET_TENANTS,
-	payload: data,
+	payload: tenants,
 	meta: { entity: TENANTS }
 });
 
+export const setTenant = tenant => ({
+	type: SET_TENANT,
+	payload: tenant,
+	meta: { entity: TENANTS }
+});
 export const createTenants = tenant => ({
 	type: CREATE_TENANTS,
 	payload: tenant,
