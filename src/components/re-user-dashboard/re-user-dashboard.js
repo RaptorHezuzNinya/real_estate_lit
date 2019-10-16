731099e33a71bd37dde88fcb62e792b96b475a36
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-16 23:41:43 +0200

    render content based on tab

--- src/components/re-user-dashboard/re-user-dashboard.js
+++ src/components/re-user-dashboard/re-user-dashboard.js
@@ -1,17 +1,18 @@
-import { LitElement, html } from 'lit-element';
+import { html } from 'lit-element';
 import { connect } from 'pwa-helpers';
 import { store } from '../../redux/store';
 import { ReUserDashboardStyles } from './re-user-dashboard-styles.js';
-
+import { PageViewElement } from '../page-view-element';
 import '../re-tenant-create-page/re-tenant-create-page.js';
 import '../re-tab-bar/re-tab-bar.js';
+import '../re-payment-upload/re-payment-upload.js';
 import '../re-tenant-overview/re-tenant-overview.js';
-import { PageViewElement } from '../page-view-element';
 
 export class ReUserDashboard extends connect(store)(PageViewElement) {
 	static get styles() {
 		return [ReUserDashboardStyles];
 	}
+
 	static get properties() {
 		return {
 			activeTab: Number
@@ -27,17 +28,28 @@ export class ReUserDashboard extends connect(store)(PageViewElement) {
 		return html`
 			<re-tab-bar @re-active-tab-change=${this.tabChanged}></re-tab-bar>
 			<p>user dashboard component (logged in)</p>
-			${this.activeTab === 0
-				? html`
-						<re-tenant-overview></re-tenant-overview>
-				  `
-				: ''}
-			${this.activeTab === 1
-				? html`
-						<re-tenant-create-page></re-tenant-create-page>
-				  `
-				: ''}
+			${this.renderPageContent()}
 		`;
+	}
+
+	renderPageContent() {
+		switch (this.activeTab) {
+			case 0:
+				return html`
+					<re-tenant-overview></re-tenant-overview>
+				`;
+			case 1:
+				return html`
+					<re-payment-upload></re-payment-upload>
+				`;
+			case 2:
+				return html`
+					<re-tenant-create-page></re-tenant-create-page>
+				`;
+
+			default:
+				break;
+		}
 	}
 
 	tabChanged(evt) {
