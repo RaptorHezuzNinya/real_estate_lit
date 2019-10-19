4436d230a88930589eeb847d5b27c43489ece092
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-19 22:23:14 +0200

    add classes conditional

--- src/components/re-tab-bar/re-tab-bar.js
+++ src/components/re-tab-bar/re-tab-bar.js
@@ -1,7 +1,8 @@
 import { LitElement, html, unsafeCSS } from 'lit-element';
-import { ReTabBarStyles } from './re-tab-bar-styles.js';
-import MDCTabBarStyles from './re-tab-bar-styles.scss';
 import { MDCTabBar } from '@material/tab-bar';
+import MDCTabBarStyles from './re-tab-bar-styles.scss';
+import { ReTabBarStyles } from './re-tab-bar-styles.js';
+import { classMap } from 'lit-html/directives/class-map.js';
 
 export class ReTabBar extends LitElement {
 	static get styles() {
@@ -10,76 +11,70 @@ export class ReTabBar extends LitElement {
 
 	static get properties() {
 		return {
-			MDCTabBar: { type: Object }
+			MDCTabBar: { type: Object },
+			tabs: Array,
+			activeTab: Number
 		};
 	}
 
 	constructor() {
 		super();
 		this.MDCTabBar = false;
+		this.tabs = false;
+		this.activeTab = 0;
 	}
 
 	firstUpdated() {
 		this.MDCTabBar = new MDCTabBar(this.shadowRoot.querySelector('.mdc-tab-bar'));
 		this.MDCTabBar.listen('MDCTabBar:activated', this.activeTabChanged.bind(this));
 	}
+
 	render() {
 		return html`
 			<div class="mdc-tab-bar" role="tablist">
 				<div class="mdc-tab-scroller">
 					<div class="mdc-tab-scroller__scroll-area">
 						<div class="mdc-tab-scroller__scroll-content">
-							<button class="mdc-tab mdc-tab--active" role="tab" aria-selected="true" tabindex="0">
-								<span class="mdc-tab__content">
-									<span class="mdc-tab__text-label">Overview</span>
-								</span>
-								<span class="mdc-tab-indicator mdc-tab-indicator--active">
-									<span
-										class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"
-									></span>
-								</span>
-								<span class="mdc-tab__ripple"></span>
-							</button>
-							<button class="mdc-tab" role="tab" aria-selected="false" tabindex="1">
-								<span class="mdc-tab__content">
-									<span class="mdc-tab__text-label">Upload payment</span>
-								</span>
-								<span class="mdc-tab-indicator mdc-tab-indicator--active">
-									<span
-										class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"
-									></span>
-								</span>
-								<span class="mdc-tab__ripple"></span>
-							</button>
-							<button class="mdc-tab " role="tab" aria-selected="false" tabindex="2">
-								<span class="mdc-tab__content">
-									<span class="mdc-tab__text-label">Create tenant</span>
-								</span>
-								<span class="mdc-tab-indicator mdc-tab-indicator--active">
-									<span
-										class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"
-									></span>
-								</span>
-								<span class="mdc-tab__ripple"></span>
-							</button>
-							<button class="mdc-tab " role="tab" aria-selected="false" tabindex="3">
-								<span class="mdc-tab__content">
-									<span class="mdc-tab__text-label">Upload tenants</span>
-								</span>
-								<span class="mdc-tab-indicator mdc-tab-indicator--active">
-									<span
-										class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"
-									></span>
-								</span>
-								<span class="mdc-tab__ripple"></span>
-							</button>
+							${this.renderTabs()}
 						</div>
 					</div>
 				</div>
 			</div>
 		`;
 	}
+	renderTabs() {
+		if (!this.tabs) {
+			return;
+		}
 
+		return this.tabs.map(tab => {
+			const active = this.activeTab === tab.tabIndex ? true : false;
+			let classesButton = {
+				'mdc-tab': true,
+				'mdc-tab--active': active
+			};
+			let classesSpan = {
+				'mdc-tab-indicator': true,
+				'mdc-tab-indicator--active': active
+			};
+			return html`
+				<button
+					class=${classMap(classesButton)}
+					role="tab"
+					aria-selected="true"
+					tabindex=${tab.tabIndex}
+				>
+					<span class="mdc-tab__content">
+						<span class="mdc-tab__text-label">${tab.label}</span>
+					</span>
+					<span class=${classMap(classesSpan)}>
+						<span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>
+					</span>
+					<span class="mdc-tab__ripple"></span>
+				</button>
+			`;
+		});
+	}
 	activeTabChanged(evt) {
 		const event = new CustomEvent('re-active-tab-change', {
 			detail: {
