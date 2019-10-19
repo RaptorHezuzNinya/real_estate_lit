323d64457aa3ac4092b3a6b0dfae6b6fa0e1671c
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-19 20:08:50 +0200

    new mw case

--- src/redux/actions/tenant.acs.js
+++ src/redux/actions/tenant.acs.js
@@ -1,25 +1,30 @@
 import { FETCH, CREATE, SET } from './action.types.js';
 
+export const TENANT = '[Tenant]';
 export const TENANTS = '[Tenants]';
 
-export const CREATE_TENANTS = `${TENANTS} ${CREATE}`;
 export const FETCH_TENANTS = `${TENANTS} ${FETCH}`;
-export const SET_TENANTS = `${TENANTS} ${SET}`;
-
 export const fetchTenants = obj => ({
 	type: FETCH_TENANTS,
 	payload: obj,
 	meta: { entity: TENANTS }
 });
 
+export const SET_TENANTS = `${TENANTS} ${SET}`;
 export const setTenants = ({ tenants, normalizeKey }) => ({
 	type: SET_TENANTS,
 	payload: tenants,
 	meta: { entity: TENANTS, normalizeKey }
 });
 
-export const createTenants = tenant => ({
-	type: CREATE_TENANTS,
-	payload: tenant,
-	meta: { entity: TENANTS }
-});
+// should always be ({data}) data = {tenants: {}}
+export const CREATE_TENANT = `${TENANT} ${CREATE}`;
+export const CREATE_TENANTS = `${TENANTS} ${CREATE}`;
+export const createTenants = ({ data, multiple }) => {
+	debugger;
+	return {
+		type: `${multiple ? CREATE_TENANTS : CREATE_TENANT}`,
+		payload: data,
+		meta: { entity: TENANTS }
+	};
+};
--- src/redux/middleware/entity/tenant.mw.js
+++ src/redux/middleware/entity/tenant.mw.js
@@ -1,18 +1,39 @@
 import { API_ERROR, API_SUCCESS, apiRequest } from '../../actions/api.acs.js';
 import { setLoader } from '../../actions/ui.acs.js';
 import { setNotification } from '../../actions/notification.acs.js';
-import { TENANTS, CREATE_TENANTS, setTenants, FETCH_TENANTS } from '../../actions/tenant.acs.js';
+import {
+	TENANT,
+	TENANTS,
+	CREATE_TENANT,
+	CREATE_TENANTS,
+	setTenants,
+	FETCH_TENANTS
+} from '../../actions/tenant.acs.js';
 
 export const tenantMiddleware = ({ dispatch, getState }) => next => action => {
 	next(action);
 
 	switch (action.type) {
-		case CREATE_TENANTS: {
+		case CREATE_TENANT: {
 			next([
 				apiRequest({
 					body: action.payload,
 					method: 'POST',
 					url: `/api/tenant`,
+					entity: TENANTS,
+					auth: true
+				}),
+				setLoader({ state: true, entity: TENANT })
+			]);
+			break;
+		}
+
+		case CREATE_TENANTS: {
+			next([
+				apiRequest({
+					body: action.payload,
+					method: 'POST',
+					url: `/api/tenants`,
 					entity: TENANTS,
 					auth: true
 				}),
