78c3b97a4fdb648d93fa54877aa5b3d7ed6c4d5d
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-19 22:24:14 +0200

    payament reducer renamed

---
+++ src/redux/reducers/payment.reducer.js
@@ -0,0 +1,15 @@
+import { SET_PAYMENTS } from '../actions/payment.js';
+
+const initialState = {
+	payments: false
+};
+
+export const paymentReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case SET_PAYMENTS:
+			return { ...state, payments: action.payload };
+
+		default:
+			return state;
+	}
+};
