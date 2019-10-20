50e09e41e20a4e9d01f9bd57a4f45eb9deb41f07
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-20 02:33:24 +0200

    acs update

--- src/redux/actions/action.types.js
+++ src/redux/actions/action.types.js
@@ -1,10 +1,14 @@
-// COMMAND - Action creator type
+// COMMAND - Action creator types
 export const UPDATE = 'Update';
 export const UPLOAD = 'Upload';
 export const CREATE = 'Create';
 export const FETCH = 'Fetch';
+export const PARSE = 'Parse';
 
-// DOCUMENT - Action creator type
+// DOCUMENT - Action creator types
 export const SET = 'Set';
 export const OPEN = 'Open';
 export const CLOSE = 'Close';
+
+// EVENT - Action creator types
+// export const COMPLETE = 'Complete';
