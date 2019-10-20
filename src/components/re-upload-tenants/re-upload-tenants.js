6f5ca79362f8ecc3b4424ed09a2b6bc88645a105
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-20 02:33:04 +0200

    update upload tenants

--- src/components/re-upload-tenants/re-upload-tenants.js
+++ src/components/re-upload-tenants/re-upload-tenants.js
@@ -1,16 +1,46 @@
 import { LitElement, html } from 'lit-element';
-
+import { connect } from 'pwa-helpers';
+import { store } from '../../redux/store';
 import { ReUploadTenantsStyles } from './re-upload-tenants-styles.js';
+import { parseFile, CSV } from '../../redux/actions/file.acs.js';
+import { TENANTS } from '../../redux/actions/tenant.acs';
+import '../re-file/re-file.js';
 
-class ReUploadTenants extends LitElement {
+class ReUploadTenants extends connect(store)(LitElement) {
 	static get styles() {
 		return [ReUploadTenantsStyles];
 	}
+
+	static get properties() {
+		return {
+			csvFile: { type: Object }
+		};
+	}
+	constructor() {
+		super();
+		this.csvFile = false;
+	}
+
 	render() {
 		return html`
-			<div><p>upload tenants component</p></div>
+			<div>
+				<br />
+				<br />
+
+				<h3>upload tenants component</h3>
+
+				<br />
+				<br />
+
+				<re-file @file-added=${this.fileAdded}></re-file>
+			</div>
 		`;
+	}
+
+	fileAdded(evt) {
+		this.csvFile = evt.detail.value;
+		store.dispatch(parseFile({ file: this.csvFile.file, fileType: CSV, subEntity: TENANTS }));
 	}
 }
 
-customElements.define('re-upload-tenants', ReUploadTenantsStyles);
+customElements.define('re-upload-tenants', ReUploadTenants);
