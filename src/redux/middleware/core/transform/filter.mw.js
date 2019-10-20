97f00382ab02dce1a7181fc42452c983b8f586da
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-21 00:41:08 +0200

    update mew middleware

--- src/redux/middleware/core/transform/filter.mw.js
+++ src/redux/middleware/core/transform/filter.mw.js
@@ -1,7 +1,21 @@
 import { FILTER } from '../../../actions/action.types.js';
+import { filterComplete } from '../../../actions/dataMutation.acs.js';
 
 export const filterMiddleware = ({ dispatch, getState }) => next => action => {
-	if (action.type.includes(FILTER) && action.meta.filterMappings) {
-		debugger;
+	if (action.type.includes(FILTER) && action.meta.identifiers) {
+		next(action);
+
+		const identifiers = action.meta.identifiers;
+
+		const result = action.payload.filter(object => {
+			if (object && identifiers.includes(object[`${action.meta.identifier}`])) {
+				return object;
+			}
+		});
+
+		const { entity, subEntity } = action.meta;
+		return dispatch(filterComplete({ entity, subEntity, result }));
 	}
+
+	next(action);
 };
--- src/redux/middleware/core/transform/transform.mw.js
+++ src/redux/middleware/core/transform/transform.mw.js
@@ -1,38 +1,28 @@
-import { TRANSFORM } from '../../actions/action.types.js';
+import { TRANSFORM } from '../../../actions/action.types.js';
 
-export const  = ({ dispatch, getState }) => next => action => {
-	if (action.type.includes(TRANSFORM)) {
-		debugger;
-		switch (action.type) {
-			case ``: {
-				break;
+export const transformMiddleware = ({ dispatch, getState }) => next => action => {
+	if (action.type.includes(TRANSFORM) && action.meta.transformMappings) {
+		next(action);
+
+		const result = action.payload.map(obj => {
+			// loop over objects in array
+			const transformMappings = action.meta.transformMappings;
+			let newObj = {};
+			for (const key in obj) {
+				if (obj.hasOwnProperty(key)) {
+					const objectKey = obj[key];
+					newObj[`${transformMappings}`];
+					// mission:
+					// make new object with the keys of new object coming from the values from transformMappingObject
+					// with the values of the new  object based on the values of the object we currently have in our loop
+					// while we use the keys of the transfromMappings object to select the correct values from the object we have in our map
+					// we need to loop the transfrom mappings object in every map iteration to build the new objects!
+				}
 			}
-		}
+
+			return newObj;
+		});
+		debugger;
 	}
 	next(action);
-};
-
-const mappings = {
-	name: 'Naam / Omschrijving', // -> "D D F VAN EDE"
-	iban: 'Rekening', // ->  "NL46INGB0701357983"
-	date: 'Datum', // ->  "20190102"
-	credited: 'Af Bij', // -> "Bij"
-	amount: ' Bedrag (EUR)', // -> "720,64"
-	memo: 'Mededelingen' // ->  "Naam: D D F VAN EDE Omschrijving: Huur jan/feb IBAN: NL15ABNA0490096379 Valutadatum: 02-01-2019"
-};
-
-const renameObjKeys = arrOfObjects => {
-	return arrOfObjects.map(obj => {
-		let creditedBool = obj['Af Bij'] === 'Bij' ? true : false;
-		let transformedItem = {
-			['name']: obj['Naam / Omschrijving'],
-			['iban']: obj['Rekening'],
-			['date']: obj['Datum'],
-			['credited']: obj['Af Bij'],
-			['amount']: obj['Bedrag (EUR)'],
-			['meno']: obj['Mededelingen']
-		};
-		transformedItem.credited = creditedBool;
-		return transformedItem;
-	});
 };
