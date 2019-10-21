8f969da10af046f0ae50ff5a6ffad1ada1d5e6bc
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-21 22:38:35 +0200

    in transfrom add renameKeys method to dynamicly rename keys!

--- src/redux/middleware/core/transform/transform.mw.js
+++ src/redux/middleware/core/transform/transform.mw.js
@@ -1,28 +1,25 @@
 import { TRANSFORM } from '../../../actions/action.types.js';
+import { transformComplete } from '../../../actions/dataMutation.acs.js';
 
 export const transformMiddleware = ({ dispatch, getState }) => next => action => {
-	if (action.type.includes(TRANSFORM) && action.meta.transformMappings) {
+	if (action.type.includes(TRANSFORM) && action.meta.keyMapping) {
 		next(action);
 
 		const result = action.payload.map(obj => {
-			// loop over objects in array
-			const transformMappings = action.meta.transformMappings;
-			let newObj = {};
-			for (const key in obj) {
-				if (obj.hasOwnProperty(key)) {
-					const objectKey = obj[key];
-					newObj[`${transformMappings}`];
-					// mission:
-					// make new object with the keys of new object coming from the values from transformMappingObject
-					// with the values of the new  object based on the values of the object we currently have in our loop
-					// while we use the keys of the transfromMappings object to select the correct values from the object we have in our map
-					// we need to loop the transfrom mappings object in every map iteration to build the new objects!
-				}
-			}
-
-			return newObj;
+			obj['Af Bij'] === 'Bij' ? (obj['Af Bij'] = true) : (obj['Af Bij'] = false);
+			return renameKeys(action.meta.keyMapping, obj);
 		});
-		debugger;
+		const { entity } = action.meta;
+		return dispatch(transformComplete({ entity, result }));
 	}
 	next(action);
 };
+
+const renameKeys = (keysMap, obj) =>
+	Object.keys(obj).reduce(
+		(acc, key) => ({
+			...acc,
+			...{ [keysMap[key] || key]: obj[key] }
+		}),
+		{}
+	);
