cebc41f47856a5b9bf4d1accedc2a0397cb69619
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-23 23:49:48 +0200

    create tenants refacotr

--- src/redux/actions/tenant.acs.js
+++ src/redux/actions/tenant.acs.js
@@ -3,6 +3,14 @@ import { FETCH, CREATE, SET } from './action.types.js';
 export const TENANT = '[Tenant]';
 export const TENANTS = '[Tenants]';
 
+export const createTenants = ({ tenants, multiple }) => {
+	return {
+		type: `${multiple ? `${TENANTS} ${CREATE}` : `${TENANT} ${CREATE}`}`,
+		payload: tenants,
+		meta: { entity: multiple ? TENANTS : TENANT }
+	};
+};
+
 export const FETCH_TENANTS = `${TENANTS} ${FETCH}`;
 export const fetchTenants = obj => ({
 	type: FETCH_TENANTS,
@@ -16,14 +24,3 @@ export const setTenants = ({ tenants, normalizeKey }) => ({
 	payload: tenants,
 	meta: { entity: TENANTS, normalizeKey }
 });
-
-// should always be ({data}) data = {tenants: {}}
-export const CREATE_TENANT = `${TENANT} ${CREATE}`;
-export const CREATE_TENANTS = `${TENANTS} ${CREATE}`;
-export const createTenants = ({ data, multiple }) => {
-	return {
-		type: `${multiple ? CREATE_TENANTS : CREATE_TENANT}`,
-		payload: data,
-		meta: { entity: TENANTS }
-	};
-};
