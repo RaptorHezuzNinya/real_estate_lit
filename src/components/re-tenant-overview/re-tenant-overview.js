51cf4247e2af52f03062e6d8d400e40e98cc20d5
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-23 23:49:11 +0200

    cleaned out component

--- src/components/re-tenant-overview/re-tenant-overview.js
+++ src/components/re-tenant-overview/re-tenant-overview.js
@@ -1,36 +1,25 @@
 import { LitElement, html } from 'lit-element';
-import { connect } from 'pwa-helpers';
-import { store } from '../../redux/store';
 import { ReTenantOverviewStyles } from './re-tenant-overview-styles.js';
 import '../re-test-card/re-test-card';
-import { paymentsByTenantId } from '../../redux/selectors/payment.selector.js';
-import { fetchUser } from '../../redux/actions/user.acs.js';
-import { fetchTenants } from '../../redux/actions/tenant.acs';
 
-export class ReTenantOverview extends connect(store)(LitElement) {
+export class ReTenantOverview extends LitElement {
 	static get styles() {
 		return [ReTenantOverviewStyles];
 	}
 	static get properties() {
 		return {
-			currentUserId: String,
 			tenants: Object
 		};
 	}
 
 	constructor() {
 		super();
-		this.currentUserId = false;
+
 		this.tenants = false;
 	}
 
-	connectedCallback() {
-		super.connectedCallback();
-		store.dispatch(fetchTenants({ currentUserId: this.currentUserId }));
-		console.log('connectedCallBack');
-	}
-
 	render() {
+		if (!this.tenants) return;
 		return html`
 			<div>
 				<h3>Tenant overview section</h3>
@@ -38,15 +27,8 @@ export class ReTenantOverview extends connect(store)(LitElement) {
 				<div class="holder">
 					${this.renderTenantCards()}
 				</div>
-				<div class="footer">
-					<re-button @button-click=${this.fetchUser} label="fetch user"></re-button>
-				</div>
 			</div>
 		`;
-	}
-
-	fetchUser() {
-		store.dispatch(fetchUser());
 	}
 
 	renderTenantCards() {
@@ -60,11 +42,6 @@ export class ReTenantOverview extends connect(store)(LitElement) {
 			}
 		}
 		return templates;
-	}
-
-	stateChanged(state) {
-		this.tenants = state.tenant.tenants;
-		this.currentUserId = state.user.user.id;
 	}
 }
 customElements.define('re-tenant-overview', ReTenantOverview);
