3a1f130f2491854f8a06ce4e82c07ce3edc579ea
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-11-05 18:05:49 +0100

    new method to render the cards

--- src/components/re-card-v2/re-card-v2.js
+++ src/components/re-card-v2/re-card-v2.js
@@ -1,44 +1,61 @@
 import { LitElement, html } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map.js';
 import { ReCardV2Styles } from './re-card-v2-styles.js';
-§
+
 export class ReCardV2 extends LitElement {
 	static get styles() {
 		return [ReCardV2Styles];
 	}
 	static get properties() {
 		return {
-			cardIsActive: { type: Boolean }
+			cardIsActive: { type: Boolean },
+			title: String,
+			supportText: String,
+			content: Array
 		};
 	}
 	constructor() {
 		super();
 		this.cardIsActive = false;
+		this.supportText = '';
+		this.content = false;
 	}
 
 	render() {
 		let classes = { card: true, cardActive: this.cardIsActive };
 		return html`
 			<div class=${classMap(classes)}>
-				<section id="cardHeader" @click=${this.handleCardActivation}>
-					
-					
-					<span class="cardHeaderTextContainer">
-						<div class="cardHeaderTitle">${this.tenant.first_name} ${this.tenant.last_name}</div>
-						<div class="cardHeaderSupportingText">${this.tenant.email}</div>
+				<section class="cardHeader" @click=${this.activateCard}>
+					<span class="container">
+						<div class="title">${this.title}</div>
+						${this.supportText
+							? html`
+									<div class="supportText">${this.supportText}</div>
+							  `
+							: ''}
 					</span>
-
 				</section>
 
-				<section class="content">
-					${this.renderPayments()}
+				<section class="cardContent">
+					${this.renderCardContent()}
 				</section>
 			</div>
 		`;
 	}
 
-	handleCardActivation() {
+	renderCardContent() {
+		if (this.content) {
+			const templates = [];
+			for (let index = 0; index < this.content.length; index++) {
+				const template = this.content[index];
+				templates.push(template);
+			}
+			return templates;
+		}
+	}
+
+	activateCard() {
 		this.cardIsActive = !this.cardIsActive;
 	}
 }
-customElements.define('re-test-card', ReCardV2Styles);
+customElements.define('re-card-v2', ReCardV2);
